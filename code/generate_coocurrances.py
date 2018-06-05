45fbf5c4643e5d15128580d306841817255c3b46
Tyler Romero <tyler.alexander.romero@gmail.com>
2018-06-05 16:52:11 -0700

    MSR model with synonym matching based on POS complete.

--- code/generate_coocurrances.py
+++ code/generate_coocurrances.py
@@ -2,10 +2,10 @@ import data_loading
 
 msr = data_loading.MSR()
 
-windows = [5, 10]
-vocab_sizes = [1000, 10000]
+windows = [5]
+vocab_sizes = [10000]
 
 for window in windows:
     for vocab_size in vocab_sizes:
         print(window, vocab_size)
-        gutenberg = msr.train_word_word_cooccurance(window, vocab_size)
+        gutenberg = msr.train_word_word_cooccurance(window, vocab_size, load=False)
