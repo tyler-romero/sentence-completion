23d8e5b61fe8765c085e99d81f398335e67c3c98
Tyler Romero <tyler.alexander.romero@gmail.com>
2018-06-12 07:53:56 -0700

    Added word-context coocurrence matrix generation for MSR

--- code/generate_coocurrences.py
+++ code/generate_coocurrences.py
@@ -2,10 +2,14 @@ import data_loading
 
 msr = data_loading.MSR()
 
-windows = [10]
-vocab_sizes = [10000, 30000]
+windows = [20]
+vocab_sizes = [10000]
 
 for window in windows:
     for vocab_size in vocab_sizes:
-        print(window, vocab_size)
-        gutenberg = msr.train_word_word_cooccurence(window, vocab_size, load=False)
+        print("Word word coocurrence: ", window, vocab_size)
+        gutenberg = msr.train_word_word_cooccurence(window, vocab_size, decay=True, load=False)
+
+# for vocab_size in vocab_sizes:
+#         print("Word context coocurrence: ", vocab_size)
+#         gutenberg = msr.train_word_context_cooccurence(vocab_size, load=False)
